6cb0dafdfafbfb1c12f72760b770cca0f3bc2faa
unknown <light@r013157.mb67.swh.mhn.de>
2022-11-19 19:44:51 +0100

    first ML conmmit

--- front/src/utils/getDay.js
+++
@@ -1,7 +0,0 @@
-const getDate = (props) => {
-  const week = ['일', '월', '화', '수', '목', '금', '토'];
-  const dayOfWeek = week[new Date(props).getDay()];
-  return dayOfWeek;
-};
-
-export default getDate;
--- front/src/utils/handleScroll.js
+++
@@ -1,8 +0,0 @@
-export const handleScroll = () => {
-  const scrollHeight = document.documentElement.scrollHeight;
-  const scrollTop = document.documentElement.scrollTop;
-  const clientHeight = document.documentElement.clientHeight;
-  if (scrollTop + clientHeight >= scrollHeight) {
-    return true;
-  }
-};
